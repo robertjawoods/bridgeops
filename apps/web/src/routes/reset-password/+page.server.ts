01c1efcf25852709ec35b19a66bf815955335d29
Robert Woods <robertjawoods@gmail.com>
2026-08-13 02:05:35 +0100

    feat: implemented password reset

---
+++ apps/web/src/routes/reset-password/+page.server.ts
@@ -0,0 +1,49 @@
+// todo: validate with zod
+
+import { auth } from '$lib/auth';
+import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
+const getResetFormData = async (event: any) => {
+    const formData = await event.request.formData();
+
+    const newPassword = formData.get('password')?.toString() ?? '';
+    const confirmation = formData.get('confirmation')?.toString() ?? '';
+    const token = formData.get('token')?.toString() ?? ''
+
+
+    return { newPassword, confirmation, token };
+};
+
+
+export const actions = {
+    reset: async (event) => {
+
+        const { newPassword, confirmation, token } = await getResetFormData(event);
+
+        console.log(token)
+
+        if (!token) {
+            return "Token is invalid"
+        }
+
+        if (newPassword !== confirmation) {
+            return "Password and confirmation must match"
+        }
+
+        const {status} = await auth.api.resetPassword({
+            body: {
+                newPassword,
+                token
+            },
+            headers: event.request.headers
+        })
+
+        // todo: notify of success
+
+        if (status) { 
+            redirect(303, '/login')
+        }
+
+    }
+} satisfies Actions;
